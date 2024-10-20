1efbf2a99401ba140121680c3a989d96ac54a960
NguyenTienAnh02 <anh40192@gmail.com>
2024-10-20 23:43:21 +0700

    Login, Register, Home page

---
+++ Backend/src/main/java/com/swp/hg/HappyProgrmmingApplication.java
@@ -0,0 +1,21 @@
+package com.swp.hg;
+
+import com.swp.hg.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class HappyProgrmmingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HappyProgrmmingApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
---
+++ Backend/src/main/java/com/swp/hg/filter/AuthenticationFilter.java
@@ -0,0 +1,72 @@
+package com.swp.hg.filter;
+
+import com.swp.hg.auth.AuthenticationRequest;
+import com.swp.hg.auth.AuthenticationResponse;
+import com.swp.hg.entity.Role;
+import com.swp.hg.entity.User;
+import com.swp.hg.repository.RoleCustomRepo;
+import com.swp.hg.repository.UserRepository;
+import com.swp.hg.service.Impl.JwtService;
+import com.swp.hg.service.Impl.UserImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final RoleCustomRepo roleCustomRepo;
+    private final JwtService jwtService;
+    private final UserImpl userImpl;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        try {
+            String userName, password;
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userName = authenticationRequest.getUsername(),
+                            password= authenticationRequest.getPassword()
+                    )
+
+            );
+            System.out.println("username: " + userName);
+            System.out.println("password: " + password);
+        } catch (AuthenticationException ex) {
+            return AuthenticationResponse.builder().message("Failed to authenticate").build();
+        }
+        User user = userImpl.takeUserByUsername(authenticationRequest.getUsername());
+
+        List<Role> role = null;
+        if (user != null) {
+            role = roleCustomRepo.getRole(user);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        Set<Role> set = new HashSet<>();
+
+        role.stream().forEach(c -> set.add(new Role(c.getName())));
+
+        user.setRoles(set);
+
+        set.stream().forEach(i -> authorities.add(new SimpleGrantedAuthority(i.getName())));
+
+        var jwtToken = jwtService.createToken(user, authorities);
+        var jwtRefreshToken = jwtService.createRefreshToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .refreshToken(jwtRefreshToken)
+                .message("Logged in successfully")
+                .build();
+    }
+}
